6424a77d7d3b25909c1de2923a3922935829d327
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-08-01 15:38:37 +0300

    added fetching to api

---
+++ src/services/EventApi.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://64c8efdcb2980cec85c19d84.mockapi.io/event'
+
+export const fetchEvents = async () => {
+    try {
+        const response = await axios.get('/events');
+        return response.data;
+    } catch (error) {
+        console.error('Произошла ошибка при обновлении', error);
+        throw error;
+    }
+}
+
+export const updateEvents = async (id, followers) => {
+  const strId = id.toString();
+  try {
+    const response = await axios.put(`/events/${strId}`, { followers });
+    return response.data;
+  } catch (error) {
+    console.error('Произошла ошибка при обновлении', error);
+    throw error;
+  }
+};
